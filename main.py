7be2845eba7d6e00322d3b3734377af6664cf14c
Anthony Torres <torresam@umich.edu>
2018-12-14 07:05:21 -0500

    add color similarity checking
    Checks the user entered color for similarity to the background to prevent invisible names

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import discord
 import logging
 import re
+import colormath
+from colormath.color_objects import sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 
+#TODO: Config file or environment variables for token
 TOKEN = ''
+DISCORD_DARK_COLOR = 0x2C2F33
+DISCORD_LIGHT_COLOR = 0xFFFFFF
+
+#TODO: Max number of colors allowed -> print a list of current colors the user can pick from
+#TODO: Clean up after bot/user messages
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -24,8 +34,16 @@ async def cleanup_roles():
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
     print('Cleaning up unused roles...')
     await cleanup_roles()
+
+    print('Creating Color objects for background similarity check...')
+    global DISCORD_DARK_COLOR
+    global DISCORD_LIGHT_COLOR
+    DISCORD_DARK_COLOR = convert_color(sRGBColor.new_from_rgb_hex('2C2F33'), LabColor)
+    DISCORD_LIGHT_COLOR  = convert_color(sRGBColor.new_from_rgb_hex('FFFFFF'), LabColor)
+
 
 @client.event
 async def on_message(message):
@@ -34,10 +52,32 @@ async def on_message(message):
 
     # Check for &hexcode
     if message.content.startswith('&'):
-        # check validness of color code
+        # Important so that we aren't duplicating upper/lowercase versions of the role
+        message.content = message.content.upper()
+
+        # Check validness of color code
+        # TODO: Maybe add support for 3 long hexcodes that would just be expanded
         if not re.search(r'^(?:[0-9a-fA-F]){6}$', message.content[1:]):
-            await message.channel.send('Invalid hex color code. Example: &fffaaa')
+            await message.channel.send('Invalid hex color code. Example: &FFFAAA')
             return
+
+        # Check for similarity to background colors so names are visible still
+        test_color = convert_color(sRGBColor.new_from_rgb_hex(message.content[1:]), LabColor)
+
+        dark_delta_e = delta_e_cie2000(test_color, DISCORD_DARK_COLOR)
+        logger.debug("Dark DeltaE Test (dark_delta_e, test_color_hex): {} {}".format(str(dark_delta_e), message.content[1:]))
+
+        light_delta_e = delta_e_cie2000(test_color, DISCORD_LIGHT_COLOR)
+        logger.debug("Light DeltaE Test (light_delta_e, test_color_hex): {} {}".format(str(light_delta_e), message.content[1:]))
+
+        if dark_delta_e <= 1.0:
+            await message.channel.send('Color is too similar to dark background. Try another color.')
+            return
+
+        if  light_delta_e <= 1.0:
+            await message.channel.send('Color is too similar to light background. Try another color.')
+            return
+
 
         role = discord.utils.get(message.guild.roles, name=message.content[1:])
         # If color exists just assign it to the user
