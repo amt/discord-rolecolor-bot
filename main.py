3004b3cdfa4a9c555333819baed0cc2cd1b8b5c6
Anthony Torres <torresam@umich.edu>
2018-12-16 23:50:15 -0500

    Rewrite role cleanup function

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ from colormath.color_diff import delta_e_cie2000
 
 # TODO: Config file or environment variables for token
 TOKEN = ''
-DISCORD_DARK_COLOR = convert_color(
-    sRGBColor.new_from_rgb_hex('2C2F33'), LabColor)
-DISCORD_LIGHT_COLOR = convert_color(
-    sRGBColor.new_from_rgb_hex('FFFFFF'), LabColor)
+DISCORD_DARK_COLOR = convert_color(sRGBColor.new_from_rgb_hex('2C2F33'), LabColor)
+DISCORD_LIGHT_COLOR = convert_color(sRGBColor.new_from_rgb_hex('FFFFFF'), LabColor)
 
 # TODO: Max number of colors allowed -> print a list of current colors the user can pick from
 # TODO: Clean up after bot/user messages
@@ -27,11 +25,10 @@ client = discord.Client(activity=discord.Game(name='&help'))
 
 
 @client.event
-async def cleanup_roles():
-    for guild in client.guilds:
-        for role in guild.roles:
-            if re.search(r'^(?:[0-9a-fA-F]){6}$', role.name) and len(role.members) == 0:
-                await role.delete(reason="Cleaning up unused roles")
+async def cleanup_roles(guild):
+    for role in guild.roles:
+        if re.search(r'^(?:[0-9a-fA-F]){6}$', role.name) and len(role.members) == 0:
+            await role.delete(reason="Cleaning up unused roles")
     return
 
 
@@ -39,7 +36,8 @@ async def cleanup_roles():
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print('Cleaning up unused roles...')
-    await cleanup_roles()
+    for guild in client.guilds:
+        await cleanup_roles(guild)
     print('Done cleaning up unused roles')
 
 
@@ -53,7 +51,7 @@ async def on_message(message):
         return
 
     if message.content == '&help':
-        await message.channel.send('Enter a hex code to get a colored name\nExample: &FFFAAA\nSee: https://www.w3schools.com/colors/colors_picker.asp')
+        await message.channel.send('Example: &FFFAAA')
         return
 
     # Important so that we aren't duplicating upper/lowercase versions of the role
@@ -62,7 +60,7 @@ async def on_message(message):
     # Check validness of color code
     # TODO: Maybe add support for 3 long hexcodes that would just be expanded
     if not re.search(r'^(?:[0-9a-fA-F]){6}$', message.content[1:]):
-        await message.channel.send('Invalid hex color code\nExample: &FFFAAA\nSee: https://www.w3schools.com/colors/colors_picker.asp')
+        await message.channel.send('Invalid color\nExample: &FFFAAA')
         return
 
     # Check for similarity to background colors so names are visible still
@@ -83,23 +81,24 @@ async def on_message(message):
         await message.channel.send('That is too similar to Discord\'s light background.\nTry another color.')
         return
 
+    # Remove old color roles
+    for role in message.author.roles:
+        if re.search(r'^(?:[0-9a-fA-F]){6}$', role.name):
+            await message.author.remove_roles(role, reason="Removing old color")
+            if len(role.members) == 1:
+                await role.delete(reason="Removing unused color")
+
     role = discord.utils.get(message.guild.roles, name=message.content[1:])
     # If color exists just assign it to the user
     if role:
         await message.author.add_roles(role, reason="Adding user's role")
         await message.channel.send('Added new role')
-
     else:
-        # Remove old color roles
-        for role in message.author.roles:
-            if re.search(r'^(?:[0-9a-fA-F]){6}$', role.name):
-                await message.author.remove_roles(role, reason="Removing old color")
-                if len(role.members) == 1:
-                    await role.delete(reason="Removing unused color")
-
         new_role = await message.guild.create_role(name=message.content[1:], color=discord.Colour(int(message.content[1:], 16)))
         await message.author.add_roles(new_role, reason="Adding user's role")
         await message.channel.send('Added new role')
+
+    await cleanup_roles(message.guild)
 
 
 # Run the bot
